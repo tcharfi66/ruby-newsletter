7e5fd62336b7275424d36aa08912431147649fa9
tcharfi66 <charfitej6@gmail.com>
2020-08-25 14:33:23 -0400

    SUCCESS

--- newsletter.rb
+++ newsletter.rb
@@ -43,7 +43,7 @@ def calculate_recipients
 end
 
 def first_n_articles(number_of_articles)
-  ARTICLES.first{number_of_articles}
+  ARTICLES.first(number_of_articles)
 end
 
 def print_recipients
@@ -66,16 +66,23 @@ def print_one_article(article)
   # Write a method that will take an article hash
   # and print the title, author and text as a formatted string
   # See the README/sample output for examples
-  puts article
+  
+  puts article[:title]
+  puts "by: " + article[:author]
+  puts article[:text]
+
 end
+
+
+
 
 def print_many_articles(articles)
   # Write a method that will take in an array of article hashes
   # and format each one using the print_one_article method
   articles.each do |art|
     print_one_article(art) 
-
   end  
+
 end
 
 def format_campus_location(campus)
@@ -111,9 +118,9 @@ end
 def run
   # We want our program to print three articles by default,
   # but we can change that number here
-  print_newsletter("3")
-  calculate_recipients
-  puts print_recipients
+  print_newsletter(3)
+  
+
   print_many_articles(ARTICLES)
 end
 
